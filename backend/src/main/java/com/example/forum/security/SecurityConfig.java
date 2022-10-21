fc8ff00c7590aff97f4ddf5b7cf2afd708ddc371
YangLi Nordic <77624563+tangshanzang@users.noreply.github.com>
2022-10-21 12:58:21 +0200

    jwt toekn and refreash token

--- backend/src/main/java/com/example/forum/security/SecurityConfig.java
+++ backend/src/main/java/com/example/forum/security/SecurityConfig.java
@@ -1,10 +1,7 @@
 package com.example.forum.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,11 +26,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        // Authentication identifies user
+        // Authorization checks if a user can/can't do certain thing
         http
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(STATELESS)
                 .and()
-                .addFilter(new CustomAuthFilter(authenticationManager()))
+                .addFilter(new CustomAuthenticationFilter(authenticationManager()))
                 .authorizeRequests()
 //                .antMatchers("/login/**","/api/user/**").permitAll()
                 .antMatchers("/login/**").permitAll()
